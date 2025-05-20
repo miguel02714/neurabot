99e4907e0dafbce95cff26531c453ecfddd8e2b9
Miguel Viana <miguelpinxs@gmail.com>
2025-05-20 00:21:55 -0300

    Atualizar o faq.py

--- faq.py
+++ faq.py
@@ -3,5 +3,6 @@ apresentacao = [
 
    
     "quemtecriou : Fui criado pela Vox Ai a empresa dona de mim, Especificamente por um grupo de programadores Braileiros, A meu nome e Neurabot"
+"quente desenvolveo : Fui desenvolvido pela Equipe de Programadores Brasileiros chamada VOX AI"
     
 ]
